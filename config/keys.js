ba70545e82ff8b54c49afb1874e653513b4753eb
thealchemist1307 <nishitrchouhan@gmail.com>
2020-02-14 19:59:45 +0530

    Backend server.js

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  googleClientId:"522520219902-jrvg774rjfahi4q5e0odio3tlq2nbc7l.apps.googleusercontent.com",
+  googleClientSecret:"gejbdo2LEWzRIKw-sUnhWLZZ"
+}
--- server.js
+++ server.js
@@ -1,1 +1,36 @@
- 
+  const express =require("express");
+  const passport = require("passport");
+  const GoogleStrategy=require("passport-google-oauth20").Strategy;
+  const keys = require("./config/keys");
+
+  const app =express();
+  const port=process.env.PORT  || 5000;
+  
+ passport.use(
+   new GoogleStrategy({
+     clientID: keys.googleClientId,
+     clientSecret: keys.googleClientSecret,
+     callbackURL: "auth/google/callback"
+        },
+       function(accessToken, refreshToken, profile, done) {
+            console.log("accessToken",accessToken);
+            console.log("refreshToken",refreshToken);
+            console.log("profile",profile);
+            console.log("done",done);
+            //  User.findOrCreate({ googleId: profile.id }, function(err, user) {
+    
+    //    return done(err, user);
+    //  });
+        }
+    )
+ );
+    app.get('/',(req,res)=>{
+        res.send("HELLO");
+    })    
+    app.get('/auth/google',passport.authenticate('google',{
+        scope:['profile','email']
+    }))
+
+    app.listen(port,()=>{
+        console.log(port);
+    })
