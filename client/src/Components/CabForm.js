8030b9ec9b741da9bbaf33c5fb73f5fba98aee94
thealchemist1307 <nishitrchouhan@gmail.com>
2020-02-17 22:35:07 +0530

    Display name working

--- client/src/Components/CabForm.js
+++ client/src/Components/CabForm.js
@@ -1,68 +1,70 @@
+
 import React from "react";
+import Calendar from "./Calendar"
+class CabForm extends React.Component {
 
-class CabForm extends React.Component
-{
-    render()
-    {
-        return(
-            <div className="column is-half">
-            <center><b>Form</b></center>
-            
 
-<div class="field">
-  <label class="label">Departure Time</label>
-  <div class="control">
+  render() {
+    return (
+      <div className="column is-half">
+        <center><b>Form</b></center>
+
+
+        <div class="field">
+          <label class="label">Departure Time</label>
+          {/* <div class="control">
     <div class="select">
       <select>
         <option>Select dropdown</option>
         <option>With options</option>
       </select>
     </div>
-  </div>
-</div>
+  </div> */}
+          <Calendar />
+        </div>
 
-<div class="field">
-  <label class="label">Message</label>
-  <div class="control">
-    <textarea class="textarea" placeholder="Textarea"></textarea>
-  </div>
-</div>
+        <div class="field">
+          <label class="label">Message</label>
+          <div class="control">
+            <textarea class="textarea" placeholder="Textarea"></textarea>
+          </div>
+        </div>
 
-<div class="field">
-  <div class="control">
-    <label class="checkbox">
-      <input type="checkbox"/>
-      I agree to the <a href="#">terms and conditions</a>
+        <div class="field">
+          <div class="control">
+            <label class="checkbox">
+              <input type="checkbox" />
+              I agree to the <a href="#">terms and conditions</a>
+            </label>
+          </div>
+        </div>
+
+        <div class="field">
+          <div class="control">
+            <label class="radio">
+              <input type="radio" name="question" />
+              Yes
     </label>
-  </div>
-</div>
-
-<div class="field">
-  <div class="control">
-    <label class="radio">
-      <input type="radio" name="question"/>
-      Yes
+            <label class="radio">
+              <input type="radio" name="question" />
+              No
     </label>
-    <label class="radio">
-      <input type="radio" name="question"/>
-      No
-    </label>
-  </div>
-</div>
+          </div>
+        </div>
 
-<div class="field is-grouped">
-  <div class="control">
-    <button class="button is-link">Submit</button>
-  </div>
-  <div class="control">
-    <button class="button is-success">Search</button>
-  </div>
-</div>
-            
-            </div>
-        )
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-link">Submit</button>
+          </div>
+          <div class="control">
+            <button class="button is-success">Search</button>
+          </div>
+        </div>
 
-    }
+      </div>
+    )
+
+  }
 }
 
 export default CabForm;
--- client/src/Components/VerticalNav.jsx
+++ client/src/Components/VerticalNav.jsx
@@ -1,86 +1,89 @@
 import React from "react";
 import Logo from "./images/logo.png";
-import {NavLink} from "react-router-dom"
-class VerticalNav extends React.Component
-{
-    render()
-    {
-        return (
-          <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-              
-              <img
-                src={Logo}
-                width="112"
-                height="100"
-              />
+import { NavLink } from "react-router-dom"
+import { connect } from 'react-redux'
+class VerticalNav extends React.Component {
+  render() {
+    return (
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
 
-              <a
-                role="button"
-                className="navbar-burger burger"
-                aria-label="menu"
-                aria-expanded="false"
-                data-target="navbarBasicExample"
-              >
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </a>
-            </div>
+          <img
+            src={Logo}
+            width="112"
+            height="100"
+          />
 
-            <div id="navbarBasicExample" className="navbar-menu">
-              <div className="navbar-start">
-                <a className="navbar-item">Home</a>
+          <a
+            role="button"
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
 
-                <a className="navbar-item">Profile</a>
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item">Home</a>
 
-                <div className="navbar-item has-dropdown is-hoverable">
-                  <a className="navbar-link">Notifications</a>
+            <a className="navbar-item">Profile</a>
 
-                  <div className="navbar-dropdown">
-                    <a className="navbar-item">About</a>
-                    <a className="navbar-item">Jobs</a>
-                    <a className="navbar-item">Contact</a>
-                    <hr className="navbar-divider" />
-                    <a className="navbar-item">Report an issue</a>
-                  </div>
-                </div>
+            <div className="navbar-item has-dropdown is-hoverable">
+              <a className="navbar-link">Notifications</a>
+
+              <div className="navbar-dropdown">
+                <a className="navbar-item">About</a>
+                <a className="navbar-item">Jobs</a>
+                <a className="navbar-item">Contact</a>
+                <hr className="navbar-divider" />
+                <a className="navbar-item">Report an issue</a>
               </div>
-              <div className="navbar-brand">
-              
-              <img
-                src={Logo}
-                width="112"
-                height="500"
-              />
+            </div>
+          </div>
+          <div className="navbar-brand">
 
-              <a
-                role="button"
-                className="navbar-burger burger"
-                aria-label="menu"
-                aria-expanded="false"
-                data-target="navbarBasicExample"
-              >
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
+            <figure class="image is-96x96" style={{ marginTop: "10px" }}>
+              <img class="is-rounded" style={{ border: "10px solid #ddd" }} src={this.props.user.avatar} />
+            </figure>
+
+            <a
+              role="button"
+              className="navbar-burger burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div className="navbar-item">
+            <div className="buttons">
+
+              <a href="/api/logout" className="button is-primary is-light is-danger" >
+                <strong>Sign Out</strong>
               </a>
             </div>
-              
-                <div className="navbar-item">
-                  <div className="buttons">
-                  
-                  <a href = "/api/logout" className="button is-primary is-light is-danger" >
-                      <strong>Sign Out</strong>
-                    </a>
-                  </div>
-                </div>
-              
-            </div>
-          </nav>
-        );
-    }
+          </div>
 
-} 
+        </div>
+      </nav>
+    );
+  }
 
-export default VerticalNav;
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.auth
+  }
+}
+
+export default connect(mapStateToProps)(VerticalNav);
