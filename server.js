756d9fae3be5c530c9a400cea6f06ec6c83dca57
thealchemist1307 <nishitrchouhan@gmail.com>
2020-02-23 19:08:42 +0530

    Beta Deployment

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ require('./routes/authRoutes')(app)
  
 if(process.env.NODE_ENV=== 'production')
 {   
-    app.use(express.static('client/build'));
+    app.use('/',express.static('client/build'));
     app.get('/',(req,res)=>{
      res.sendFile(path.resolve(__dirname,'client','build','index.html'));   
     });
@@ -52,7 +52,7 @@ if(process.env.NODE_ENV=== 'production')
 // })   
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client","build", "index.html"));
 });
 
 app.listen(port,()=>{
