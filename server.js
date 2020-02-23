6e5980896ebbf771973d9864a11f919db3d77cd6
thealchemist1307 <nishitrchouhan@gmail.com>
2020-02-23 19:42:20 +0530

    Beta Deployment

--- server.js
+++ server.js
@@ -50,10 +50,14 @@ if(process.env.NODE_ENV=== 'production')
 // app.get('/',(req,res)=>{
 //     res.send("HELLO");
 // })   
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "client","build", "index.html"));
-});
+// app.use('/',express.static(path.join(__dirname, "build")));
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "client","build", "index.html"));
+// });
+ app.use("/", express.static("client/build"));
+ app.get("/", (req, res) => {
+   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+ });
 
 app.listen(port,()=>{
     console.log(port);
