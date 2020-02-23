50237fb15ef8f53aa42c40fdaa3915e169208bc1
thealchemist1307 <nishitrchouhan@gmail.com>
2020-02-23 19:51:43 +0530

    Beta Deployment

--- server.js
+++ server.js
@@ -41,10 +41,12 @@ require('./routes/authRoutes')(app)
  
 if(process.env.NODE_ENV=== 'production')
 {   
-    app.use('/',express.static('client/build'));
-    app.get('/',(req,res)=>{
-     res.sendFile(path.resolve(__dirname,'client','build','index.html'));   
-    });
+     app.use(express.static(path.join(__dirname, "client/build")));
+
+     // Handle React routing, return all requests to React app
+     app.get("*", function(req, res) {
+       res.sendFile(path.join(__dirname, "client/build", "index.html"));
+     });
 }
 
 // app.get('/',(req,res)=>{
@@ -55,7 +57,7 @@ if(process.env.NODE_ENV=== 'production')
 //   res.sendFile(path.join(__dirname, "client","build", "index.html"));
 // });
  app.use("/", express.static("client/build"));
- app.get("/", (req, res) => {
+ app.get("*", (req, res) => {
    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
  });
 
