c5bcff6640b1672f3f04d17623bd0879e804b54a
Karthik Shetty <45932358+karthikshetty03@users.noreply.github.com>
2020-03-23 01:54:08 +0530

    Delete dev.js

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientId: "610868745041-dgrnfspmeir73kbip8clvv3c5vcv32ea.apps.googleusercontent.com",
-  googleClientSecret: "xjRdYC8rzGMDNOGHcaf0xvH2",
-  mongoURI: "mongodb+srv://admin:softwareengineering@bphc-unite-3i8dv.mongodb.net/test?retryWrites=true&w=majority",
-  cookieKey: "abcdefghijkl"
-};
